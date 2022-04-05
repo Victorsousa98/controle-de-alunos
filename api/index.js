92bba47e204699a3c605c58a69328ac8c827dc8c
Victorsousa98 <victorsousa708@gmail.com>
2022-04-04 21:03:21 -0300

    adiciona dependencias

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express'); // express é um framework para criação de aplicações web
+const bodyParser = require('body-parser');//body-parse é um módulo que permite que o express entenda o formato json
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3000;
+
+app.get("test", (req, res) => {
+    res.send("Hello World");
+});
+
+
+
+//Método do express que permite que o servidor seja iniciado
+    app.listen(port, () => console.log(`Servidor rodando na porta ${port}!`));
+
+module.exports = app;
