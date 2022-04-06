481e20179be3372c3c8697449e9fd125fe5b2c19
Victorsousa98 <victorsousa708@gmail.com>
2022-04-05 23:55:11 -0300

    adiciona Busca por id de pessoas

--- controllers/PessoaController.js
+++ controllers/PessoaController.js
@@ -3,8 +3,23 @@ const dataBase = require('../models/index.js');
 class PessoaController {
     static async pegarTodasAsPessoas(req, res) {
         try{
-            const todasAsPessoas = await dataBase.Pessoas.findAll();
+            const todasAsPessoas = await dataBase.Pessoas.findAll();//usa findAll para buscar todos os registros
             return res.status(200).json(todasAsPessoas);
+        }catch(err){
+            return res.status(500).json(
+                err.message
+            );
+        }
+    }
+
+    static async pegarPessoaPorId(req, res) {
+        try{
+            const pessoa = await dataBase.Pessoas.findOne({//usar findONe para buscar apenas um registro
+                where: {
+                    id: req.params.id//busca o id passado na url
+                }
+            });
+            return res.status(200).json(pessoa);
         }catch(err){
             return res.status(500).json(
                 err.message
--- routes/pessoasRoute.js
+++ routes/pessoasRoute.js
@@ -4,5 +4,6 @@ const PessoaController = require('../controllers/PessoaController');
 const router = Router();
 
 router.get('/pessoas', PessoaController.pegarTodasAsPessoas);
+router.get('/pessoas/:id', PessoaController.pegarPessoaPorId);
 
 module.exports = router;
